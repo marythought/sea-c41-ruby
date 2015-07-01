c9d4ec9113cf007c041b2e9d9d24d1926f4190a0
Mary Dickson <mdickson@gmail.com>
2015-06-30 18:53:31 -0700

    fixed spacing issue in #7

--- lib/class2/exercise7.rb
+++ lib/class2/exercise7.rb
@@ -24,15 +24,15 @@
 line_width = 50
 puts('Table of Contents'.center(line_width))
 puts('Chapter 1:  Getting Started'.ljust(line_width / 2) +
-     'page  1'.rjust(line_width / 2))
+     'page  1'.rjust((line_width / 2) -2))
 puts('Chapter 2:  Numbers'.ljust(line_width / 2) +
      'page  9'.rjust(line_width / 2))
 puts('Chapter 3:  Letters'.ljust(line_width / 2) +
      'page 13'.rjust(line_width / 2))
 
-line_width = 40
-str = '--> text <--'
-puts(str.ljust(line_width))
-puts(str.center(line_width))
-puts(str.rjust(line_width))
-puts(str.ljust(line_width / 2) + str.rjust(line_width / 2))
+# line_width = 40
+# str = '--> text <--'
+# puts(str.ljust(line_width))
+# puts(str.center(line_width))
+# puts(str.rjust(line_width))
+# puts(str.ljust(line_width / 2) + str.rjust(line_width / 2))
